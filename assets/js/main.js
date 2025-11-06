11c1618beef2e222e00e0403feab16ebb72370d7
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2025-11-06 18:50:44 +0500

    added the remove duplicate from an array mehtod

--- assets/js/main.js
+++ assets/js/main.js
@@ -299,3 +299,25 @@ const reversedArray = (arr) => {
 };
 
 console.log(reversedArray([1, 2, 3, 4, 5]));
+
+const removeDuplicates = (arr) => {
+  let unique = [];
+  for (let i = 0; i < arr.length; i++) {
+    let exist = false;
+    for (let j = 0; j < unique.length; j++) {
+      if (arr[i] === unique[j]) {
+        exist = true;
+        break;
+      }
+    }
+    if (!exist) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
+};
+
+console.log(
+  "This is removed duplicates:",
+  removeDuplicates([1, 2, 3, 2, 4, 1, 5])
+);
